9735b8bf93a292dca0a03ffb7b43fa8ecbab8f25
kildem <kilden777@gmail.com>
2017-04-06 18:00:09 +0200

    mock http api

--- src/index.test.js
+++ src/index.test.js
@@ -9,11 +9,11 @@ describe('test', () => {
 });
 
 describe('index.html', () => {
-	it('shouldj say hello', (done) => {
+	it('should have h1 says Users', (done) => {
 		const index = fs.readFileSync('./src/index.html', 'utf-8');
 		jsdom.env(index, function (err, window) {
 			const h1 = window.document.getElementsByTagName('h1')[0];
-			expect(h1.innerHTML).to.equal("Hello!");
+			expect(h1.innerHTML).to.equal("Users");
 			done();
 			window.close();
 		})
